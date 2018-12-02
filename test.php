d4dc7f19c4a844df496e553e74ac69f288a4961e
alemasle <alexislm35@outlook.fr>
2018-12-02 14:58:04 +0100

    stockage

---
+++ test.php
@@ -0,0 +1,39 @@
+<html>
+<body bgcolor="#FFFFFF" text="#000000" link="#006600" vlink="#669966" alink="#990000">
+Votre mission : entrer le nom de la personne qui a pris "kiki taxi" le 16/09/2014.
+
+Le challenge se trouve <a href="index2.php">ici</a> !
+
+<FORM action="index.php" method=get>
+<BR><BR>
+<div align=center>
+	<TABLE width=500>
+	<TBODY>
+	<TR>
+		<TD align=right>Nom de la personne :</TD>
+		<TD><INPUT class=intext maxLength=30 name=nom></TD>
+	</TR>
+	<TR>
+		<TD colspan=2 align=center><br></b><INPUT class="buttons" type=submit value="Verifier"></TD>
+	</TR>
+			
+	</TBODY>
+	</TABLE>
+<?php
+
+$ligne= array ('aaa', 'abc');
+$nom = 'aaa';
+if ($ligne[0]==$nom)
+	{
+	echo "Joli coup ! Vous avez un voyage gratuit chez Kiki Taxi !<BR>";
+	echo "</div></form></body></html>";
+		exit;
+	}
+?>
+
+</div>
+</form>
+<BR>
+</body>
+</html>
+
